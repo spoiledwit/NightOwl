1249ffef08722db01a26e7645bd9d50c98efe2da
Ahmad Ashfaq <spoiledwit@gmail.com>
2023-05-14 00:50:36 +0500

    Added pusher to the message body

---
+++ app/libs/pusher.ts
@@ -0,0 +1,15 @@
+import PusherServer from "pusher"
+import PuserClient from "pusher-js"
+
+export const pusherServer = new PusherServer({
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    secret: process.env.PUSHER_SECRET!,
+    cluster: "ap2",
+    useTLS: true
+});
+
+
+export const pusherClient = new PuserClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, {
+    cluster: "ap2",
+});
